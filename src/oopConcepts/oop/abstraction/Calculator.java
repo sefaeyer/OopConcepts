57c961e88231597306db10a0eec8b237d877768f
Sefa Eyer <eyersefa@gmail.com>
2024-04-08 01:51:32 +0300

    v2 Abstraction 08/04

--- src/oopConcepts/oop/abstraction/Calculator.java
+++ src/oopConcepts/oop/abstraction/Calculator.java
@@ -6,4 +6,18 @@ public class Calculator {
     public int add(int num1, int num2){
         return num1+num2;
     }
+
+    //calisan kodun arka tarafinda detaylara ihtiyacimiz yok
+    //arabanin motor sistemleri nasil calisiyor, bilmemize gerek yok
+    //hesap makinesinde toplama methodunun nasil calistigini bilmeme gerek yok
+
+    //abstraction : soyutlama
+
+    // -> modulerlik katar
+    // -> esneklik katar
+    // -> karisikligi azaltir
+    // -> kodun tekrar kullanılabilirliğini artırır
+    // -> bakimi kolaylastirir
+
+
 }
